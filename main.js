caf3ed485fdb6d4ad3c79c4f7f25e2bc98d9e1bb
Jacques Etienne <santiagondiaye1@gmail.com>
2021-11-26 09:43:08 +0000

    Fixed: Making the active class to toggle smoothly

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-var home = document.querySelector(".home");
-var verticalLink = document.querySelectorAll(".vertical-links");
-var i;
+var verticalNav = document.querySelectorAll(".vertical-nav-link");
+var activeClass = document.getElementsByClassName("active");
 
-home.addEventListener('click', function(e){
-    if(home.parentNode.classList.length == 1){
-        for(i = 0; i < verticalLink.length; i ++){
-            if(verticalLink[i].parentNode.classList.length > 1){
-                verticalLink[i].parentNode.classList.remove("active");
-                home.parentNode.classList.add("active");
-            }
-        }
-    }
-});
+for (var i = 0; i < verticalNav.length; i++){
+    verticalNav[i].addEventListener('click', function(e){ 
+        activeClass[0].classList.remove('active');
+        e.currentTarget.parentNode.classList.add('active');
+    });
+}
